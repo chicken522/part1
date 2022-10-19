fbc2593e1b85de0a09db0a4f69dc2e82b5b82851
daji <1281429158@qq.com>
2022-10-19 14:14:46 +0800

    增加Note.js组件

--- src/App.js
+++ src/App.js
@@ -1,61 +1,20 @@
-const Hello = (props) => {
-  return (
-    <>
-      <p>Hello {props.name}, you are {props.age} years old </p>
-    </>
-  )
-}
+import Note from "./components/Note"
 
-const App = () => {
+const App = ({ notes }) => {
 
-  const name = "Peter"
-  const age = 18
-
-
-  const t = [1,2,3,4,5]
-
-  const m1 = t.map(value => value*2)
-  console.log(m1)
-
-  const m2 = t.map(value => '<li>' + value +"</li>")
-  console.log(m2)
-
-  const [first,second, ...rest] = t
-  console.log(first,second)
-  console.log(rest)
-  
-  const object1 = {
-    name: "Artho test",
-    age: 35,
-    education: "PhD"
-
-  }
-
-  // const sum = function(p1,p2){
-  //   console.log(p1)
-  //   console.log(p2)
-  //   return p1 + p2
-  // }
-
-
-  // const sum = p => { 
-  //   return p
-  // }
-
-  const sum = p => p*p
-
-  const result = sum(5)
+  const result = notes.map(note => note.id)
   console.log(result)
-
-
   return (
-    <>
-      <h1>Greatings</h1>
-      <Hello name="Maya" age={26+10}/>
-      <Hello name={name} age={age}/>
-    </>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note}></Note>
+        )}
+      </ul>
+    </div>
   )
 }
 
 
-export default App;
+export default App
